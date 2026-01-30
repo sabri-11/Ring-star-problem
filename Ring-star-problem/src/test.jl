fe3b5201373089585ffaa3be421a73bd08fef0b4
Sabri <sabrirebaine@gmail.com>
2026-01-30 01:36:02 +0100

    tests

--- Ring-star-problem/src/test.jl
+++ Ring-star-problem/src/test.jl
@@ -30,8 +30,52 @@ end
 
 function test_metaHeuristiqueGloutonne(p, nbTests)
 
-    println("Coût moyen : $(round(coutMoyen, digits=2))")
-    println("Temps moyen : $tempsMoyen")
+    coutMoyen = 0.0
+    tempsMoyen = 0.0
+
+    for i in 1:nbTests
+        res = @timed ae_ppv_metaHeuristiqueGlouton(p, "../tspFile/att48.tsp", 0)
+        temps = res.time
+        cout = res.value[4]
+        print("(cout$i=$cout, temps$i=$temps)\t")
+        coutMoyen += cout
+        tempsMoyen += temps
+        
+    end
+
+    coutMoyen /= nbTests
+    tempsMoyen /= nbTests
+    
+    println()
+    println("Coût moyen sur $nbTests tests pour p=$p: $(round(coutMoyen, digits=2))")
+    println("Temps moyen sur $nbTests tests pour p=$p : $tempsMoyen")
 
 
 end
+
+
+function test_metaHeuristiqueRandom100iter(p, nbTests)
+
+    coutMoyen = 0.0
+    tempsMoyen = 0.0
+
+    for i in 1:nbTests
+        res = @timed ae_ppv_metaHeuristiqueRandom(p, "../tspFile/att48.tsp", 100, 0)
+        temps = res.time
+        cout = res.value[4]
+        print("(cout$i=$cout, temps$i=$temps)\t")
+        coutMoyen += cout
+        tempsMoyen += temps
+        
+    end
+
+    coutMoyen /= nbTests
+    tempsMoyen /= nbTests
+    
+    println()
+    println("Coût moyen sur $nbTests tests pour p=$p: $(round(coutMoyen, digits=2))")
+    println("Temps moyen sur $nbTests tests pour p=$p : $tempsMoyen")
+
+
+end
+
